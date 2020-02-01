f932f05e3fe047ff8612973179e82b80f986c866
Yucheng Shi <yuchengs@uchicago.edu>
2020-01-31 23:08:53 -0600

    update build file

---
+++ precache-manifest.53b55d75aa2c15d162fae88f2acde4c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "49e0834f39dbb2ae5769e3d55b4e5683",
+    "url": "/home/static/media/cv.49e0834f.pdf"
+  },
+  {
+    "revision": "39f5226f4c93395f80543581a447a37b",
+    "url": "/home/static/media/IMG_2129.39f5226f.JPG"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "2e496556c3b7fb6ad345",
+    "url": "/home/static/js/main.b6bc562f.chunk.js"
+  },
+  {
+    "revision": "4c21de97380a94039769",
+    "url": "/home/static/js/2.a26dec71.chunk.js"
+  },
+  {
+    "revision": "2e496556c3b7fb6ad345",
+    "url": "/home/static/css/main.71667d0a.chunk.css"
+  },
+  {
+    "revision": "4c21de97380a94039769",
+    "url": "/home/static/css/2.db21bf8d.chunk.css"
+  },
+  {
+    "revision": "a5ec87cc0effbe82daa2aab1463e7cd4",
+    "url": "/home/index.html"
+  }
+];
